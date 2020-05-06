5c38bdcd29919ffef495cd453f1acb412b225e20
Howley <3howley@gmail.com>
2020-05-05 22:43:53 -0400

    Add '$news' functionality to bot

--- bot.py
+++ bot.py
@@ -1,4 +1,10 @@
+# Dependencies
 import discord
+
+# Local dependencies
+from modules.csv import CSV
+from modules.news import News
+from modules.discordhelp import DiscordHelp
 
 client = discord.Client()
 
@@ -8,8 +14,18 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
     if message.author == client.user:
         return
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+    if message.content.startswith('$news'):
+        result = News.getNews(message)
+
+        if not DiscordHelp.isValidLength(result):
+            result = "**Too many articles to list**"
+
+        await message.channel.send(result)
+
