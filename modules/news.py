1dc7421e2036faa2a1fb5524c4c46221e1bae3f2
Howley <3howley@gmail.com>
2020-05-06 20:51:27 -0400

    Use RSS feed instead of web scraping
    This commit changes the news scraper to use RSS feeds instead of
    scraping a webpage. This will make adding new websites much easier, as
    everything is standardized within the feedparser package

--- modules/news.py
+++ modules/news.py
@@ -1,124 +1,35 @@
-from bs4 import BeautifulSoup
 from datetime import date, datetime
 from string import Formatter
-import requests
+from time import mktime
+
+import feedparser
 
 class News:
-    # Sometimes requests doesn't work unless we have some specific headers *shrug*
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'}
-
     # This absolutely SUCKS i hate it
     # I'll figure out a way to make this better, maybe make it an external file
     help_message = '```$news - get cybersecurity news\n\nFormat is $news website [num]\n\nwebsite         website target\n[num]           number of articles to fetch\n                default: 5```'
 
-    # Website Scraping Functions
+    # Feed URLs
+    bleepingComputer = "https://bleepingcomputer.com/feed/"
+    darkReading = "https://www.darkreading.com/rss_simple.asp"
 
+    # Get list of articles from url
     @staticmethod
-    def getSoup(url):
-        site = requests.get(url, headers=News.headers)
-        coverpage = site.content
-        soup = BeautifulSoup(coverpage, 'lxml')
-
-        return soup
-
-    @staticmethod
-    def darkReading():
-        articles_list = []
-
-        url = "https://darkreading.com/"
-
-        soup = News.getSoup(url)
-
-        # Get list of articles
-        articles = soup.find('div', id='left-column-inner')
-
-        today = date.today()
-
-        for article in articles.find_all('header', class_='strong medium'):
-            data = article.findNextSibling('div', style='overflow: hidden;')
-
-            date_str = str(data.find('span', class_='allcaps smaller').next_sibling).split(',')[-1].strip()
-            article_date = datetime.strptime(date_str, '%m/%d/%Y').date()
-            if article_date < today:
-                # We have hit the end of today's articles
-                break
-
-            info = {}
-
-            info['date'] = str(article_date)
-
-            link = article.find('a')['href']
-            info['link'] = 'https://darkreading.com' + link
-
-            info['title'] = article.find('a')['title']
-
-            description = data.find('span', class_='black smaller')
-            info['description'] = description.text
-
-            author = data.find('a', class_='color-link')
-            info['author'] = author.text
-
-            articles_list.append(info)
-
-        return articles_list
-
-
-    # Get today's news from bleepingcomputer.com
-    @staticmethod
-    def bleeping_computer():
-        # This will hold the data points from each article
-        articles_list = []
-
-        url = "https://bleepingcomputer.com"
-
-        soup = News.getSoup(url)
-
-        # Extract every article on the page
-        articles = soup.find_all('div', class_='bc_latest_news_text')
-
-        today = date.today()
-
-        # This is a little janky, but I'm still trying to figure out bs4
-        # Add data to
-        for article in articles:
-            date_str = article.find('li', class_='bc_news_date').text
-            article_date = datetime.strptime(date_str, '%b %d, %Y').date()
-            if article_date < today:
-                continue
-
-            info = {}
-
-            info['date'] = str(article_date)
-
-            title = article.contents[3].a
-            link = title['href']
-            info['link'] = link
-
-            title = title.text
-            info['title'] = title
-
-            description = article.contents[5].text
-            info['description'] = description
-
-            author = article.contents[7].a.text
-            info['author'] = author
-
-            articles_list.append(info)
-
-        return articles_list
+    def getEntries(url):
+        return feedparser.parse(url)['entries']
 
     @staticmethod
     def getNewsList(site):
         if site == "bleepingcomputer":
-            return News.bleeping_computer()
+            return RSS.getEntries(News.bleepingComputer)
         elif site == "darkreading":
-            return News.darkReading()
+            return RSS.getEntries(News.darkReading)
         else:
             raise UnknownSiteError(site)
 
     @staticmethod
     def getNews(message):
-        # format is "$news site number", where number is the number
+        # format is "$news site [number]", where number is the number
         # of articles to return, because discord has a character limit
         # of 2000. You could make it send multiple messages but whatever.
         split_message = message.content.split(' ')
@@ -148,7 +59,12 @@ class News:
 
         formatter = Formatter()
         responses = []
+        today = date.today()
         for i, article in enumerate(articles):
+            date_ = date.fromtimestamp(mktime(article['published_parsed']))
+            if today > date_:
+                break
+
             response = formatter.format("{num}. {title} by {author} ({link})", num=i+1, \
                                         title=article['title'], author=article['author'], \
                                         link=article['link'])
